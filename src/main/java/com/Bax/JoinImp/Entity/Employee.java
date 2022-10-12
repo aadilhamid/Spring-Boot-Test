aab7d77393b0a6436c5b2d3373a37ce3ec2a4c9f
Aadil Bhat <adilhamid19@yahoo.com>
2022-10-12 14:46:48 +0530

    Pincode added to Adress Entity

---
+++ src/main/java/com/Bax/JoinImp/Entity/Employee.java
@@ -0,0 +1,28 @@
+package com.Bax.JoinImp.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+@Id
+@GeneratedValue
+    private Long empId;
+    private String name;
+    private Integer salary;
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name = "address_add_id")
+    private Address address;
+
+
+
+
+}
---
+++ src/main/java/com/Bax/JoinImp/JoinImpApplication.java
@@ -0,0 +1,13 @@
+package com.Bax.JoinImp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinImpApplication.class, args);
+	}
+
+}
